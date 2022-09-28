d1bb54288d1cc49072dc44feee498510fc706c22
viniciuslimaavila <vinicius.lima.avila@hotmail.com>
2022-09-28 15:06:28 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/projeto_resumo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projeto_resumo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Resumo dos guris",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resumo dos guris"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(""),
+        ),
+      ),
+    );
+  }
+}
